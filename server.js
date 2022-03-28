605981fccbd3ba3628769b4622b3271395c57e7e
patfern <patrick.j.fernbach@gmail.com>
2022-03-28 17:00:39 -0400

    login logout

--- server.js
+++ server.js
@@ -7,6 +7,21 @@ const exphbs = require('express-handlebars');
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+const session = require('express-session');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
+const sess = {
+  secret: 'Super secret secret',
+  cookie: {},
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize
+  })
+};
+
+app.use(session(sess))
+
 const hbs = exphbs.create({});
 
 app.use(express.json());
